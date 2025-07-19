f4825a9956cefa71cfa92d2c56d31c5f45035f61
raiwyzs <161459392+raiwyzs@users.noreply.github.com>
2025-07-19 13:03:13 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS funcionarios;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS funcionarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL
+    email TEXT NOT NULL UNIQUE,
+    telefone TEXT NOT NULL,
+    categoria TEXT NOT NULL,
+    senha TEXT NOT NULL
 );
